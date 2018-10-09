3380659ed9f7c830ce27a94aa316ff786775459b
WLLMGS <willem.goos@hotmail.com>
2018-10-09 20:30:23 +0200

    made bullets explode, added smoke prefab

--- Assets/Scripts/FireballMovementBehavior.cs
+++ Assets/Scripts/FireballMovementBehavior.cs
@@ -2,26 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireballMovementBehavior : MonoBehaviour {
+public class FireballMovementBehavior : MonoBehaviour
+{
 
-	private float _moveSpeed = 8.0f;
+    private float _moveSpeed = 8.0f;
 
-	private Rigidbody2D _rigid;
+    private Rigidbody2D _rigid;
 
-	private ProjectileStatsScript _stats;
-	// Use this for initialization
-	void Start () {
-		_rigid = GetComponent<Rigidbody2D>();
-	_stats = GetComponent<ProjectileStatsScript>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		HandleMovement();
-	}
+    private ProjectileStatsScript _stats;
 
-	void HandleMovement()
-	{
-		_rigid.velocity = transform.right * _moveSpeed * _stats.Direction;
-	}
+    [SerializeField] private GameObject _smoke;
+    // Use this for initialization
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _stats = GetComponent<ProjectileStatsScript>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        _rigid.velocity = transform.right * _moveSpeed * _stats.Direction;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Enemy")
+        {
+            Explode();
+            Destroy(gameObject);
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(_smoke, transform.position + new Vector3(0,0,-5), Quaternion.identity);
+    }
 }
--- Assets/Scripts/HoverRobotBehavior.cs
+++ Assets/Scripts/HoverRobotBehavior.cs
@@ -2,56 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoverRobotBehavior : MonoBehaviour {
+public class HoverRobotBehavior : MonoBehaviour
+{
 
 
-	private float _moveSpeed = 2.5f;
-	private GameObject _player;
-	private bool _doFollowPlayer = false;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		GoToPlayer();
-	}
+    private float _moveSpeed = 2.5f;
+    private GameObject _player;
+    private bool _doFollowPlayer = false;
+    // Use this for initialization
+    void Start()
+    {
 
-	void OnTriggerEnter2D(Collider2D collider)
-	{
-		if(collider.tag == "Player")
-		{
-			if(_player == null) _player = collider.gameObject;
-			PlayerDetected();
-		}
-	}
+    }
 
-	void OnTriggerExit2D(Collider2D collider)
-	{
-		if(collider.tag == "Player")
-		{
-			_doFollowPlayer = false;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        GoToPlayer();
+    }
 
-	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            if (_player == null) _player = collider.gameObject;
+            PlayerDetected();
+        }
+    }
 
-	void PlayerDetected()
-	{
-		_doFollowPlayer = true;
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            _doFollowPlayer = false;
+        }
 
-		if(_player.transform.position.x < transform.position.x)
-		{
-			transform.localScale = new Vector3(-1,1,1);
-		}
-		else transform.localScale = new Vector3(1,1,1);
-	}
+    }
 
-	void GoToPlayer()
-	{
-		if(_doFollowPlayer)
-		{
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(_player.transform.position.x, transform.position.y, transform.position.z), Time.deltaTime * _moveSpeed);
-		}
-	}
+    void PlayerDetected()
+    {
+        _doFollowPlayer = true;
+
+        if (_player.transform.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    void GoToPlayer()
+    {
+        if (_doFollowPlayer)
+        {
+            float distance = Mathf.Abs(transform.position.x - _player.transform.position.x);
+
+            if (distance > 1.0f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(_player.transform.position.x, transform.position.y, transform.position.z), Time.deltaTime * _moveSpeed);
+            }
+        }
+    }
+
 }
