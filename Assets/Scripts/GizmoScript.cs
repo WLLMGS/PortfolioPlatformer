7cebbb74cf45f2438d7e733b402a502d5feaf02e
WLLMGS <willem.goos@hotmail.com>
2018-10-16 18:04:29 +0200

    added gizmos to make screens visible

---
+++ Assets/Scripts/GizmoScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoScript : MonoBehaviour
+{
+
+	[SerializeField] Color _gizmoColor = new Color(1,0,0,0.5f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = _gizmoColor;
+        //Gizmos.DrawSphere(transform.position, 1.0f);
+
+		Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
